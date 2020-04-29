1ca1c3acda414fd1bf77ccdb8ff972d2c399e1ea
Roberto Seidenberg <roberto.giessler@googlemail.com>
2020-04-29 19:08:20 +0200

    Hides video on mobile browsers

--- js/main.js
+++ js/main.js
@@ -5,4 +5,13 @@ $(document).ready(function() {
 	} else {
 		$("body").css("background-color", "#cccccc");
 	}
+
+	var ua = window.navigator.userAgent;
+	var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+	var webkit = !!ua.match(/WebKit/i);
+	var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
+
+	if (iOSSafari == false) {
+		$("video").css("display", "none")
+	}
 });
