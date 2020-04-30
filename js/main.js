35931884e29f3bcb28ca8d6e6aaed889c3e3b491
Roberto Seidenberg <roberto.giessler@googlemail.com>
2020-04-30 09:20:12 +0200

    Adds preview as tab

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
 
-	if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
-    	$("body").css("background-color", "#d2d2d2");
-	} else {
-		$("body").css("background-color", "#cccccc");
-	}
-
 	var ua = window.navigator.userAgent;
 	var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
 	var webkit = !!ua.match(/WebKit/i);
